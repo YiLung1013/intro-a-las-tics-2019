321e1fec94098b0f521709ae3ac470892aed5f69
YiLung1013 <56326995+YiLung1013@users.noreply.github.com>
2019-10-28 18:20:46 -0600

    2019/10/28

--- index.php
+++ index.php
@@ -43,22 +43,64 @@ width: auto;;
 <body>
 
 
-<!-- Outline button (default colors) -->
-			<div class="contentindex">
+				<!-- Outline button (default colors) -->
 				<div class="row">
 					<div class="col-lg-4">
 						<div class="card card-body border-top-primary">
 							<div class="text-center">
-								<h6>Entrada</h6>
+								<h6 class="m-0 font-weight-semibold">Default colors</h6>
+								<p class="text-muted mb-3">Default 8 contextual colors</p>
 
-		                    	<button type="button" class="btn btn-outline-primary">Sistema</button>
-		                    	<button type="button" class="btn bg-transparent text-primary border-primary ml-1">Mi Sitio</button>
+		                    	<button type="button" class="btn btn-outline-primary">Hoverable</button>
+		                    	<button type="button" class="btn bg-transparent text-primary border-primary ml-1">Static</button>
 	                    	</div>
 						</div>
 					</div>
+
+					<div class="col-lg-4">
+						<div class="card card-body border-top-primary">
+							<div class="text-center">
+								<h6 class="m-0 font-weight-semibold">Default colors with icon</h6>
+								<p class="text-muted mb-3">Available in both directions</p>
+
+		                    	<button type="button" class="btn btn-outline-danger"><i class="icon-cog3 mr-2"></i> Hoverable</button>
+		                    	<button type="button" class="btn bg-transparent text-danger border-danger ml-1"><i class="icon-cog3 mr-2"></i> Static</button>
+	                    	</div>
+						</div>
+					</div>
+
+					<div class="col-lg-4">
+						<div class="card card-body border-top-primary">
+							<div class="text-center">
+								<h6 class="m-0 font-weight-semibold">Default colors with menu</h6>
+								<p class="text-muted mb-3">Outline button with dropdown</p>
+
+								<div class="btn-group">
+			                    	<button type="button" class="btn btn-outline-success dropdown-toggle" data-toggle="dropdown">Hoverable</button>
+			                    	<div class="dropdown-menu dropdown-menu-right">
+										<a href="#" class="dropdown-item"><i class="icon-menu7"></i> Action</a>
+										<a href="#" class="dropdown-item"><i class="icon-screen-full"></i> Another action</a>
+										<a href="#" class="dropdown-item"><i class="icon-mail5"></i> One more action</a>
+										<div class="dropdown-divider"></div>
+										<a href="#" class="dropdown-item"><i class="icon-gear"></i> Separated line</a>
+									</div>
+								</div>
+
+								<div class="btn-group ml-1">
+			                    	<button type="button" class="btn bg-transparent text-success border-success dropdown-toggle" data-toggle="dropdown">Static</button>
+			                    	<div class="dropdown-menu dropdown-menu-right">
+										<a href="#" class="dropdown-item"><i class="icon-menu7"></i> Action</a>
+										<a href="#" class="dropdown-item"><i class="icon-screen-full"></i> Another action</a>
+										<a href="#" class="dropdown-item"><i class="icon-mail5"></i> One more action</a>
+										<div class="dropdown-divider"></div>
+										<a href="#" class="dropdown-item"><i class="icon-gear"></i> Separated line</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
-<!-- /outline button (default colors) -->
+				<!-- /outline button (default colors) -->
 
 </body>
 </html>

8072a997643526b252340bb96675379118ed887c
YiLung1013 <56326995+YiLung1013@users.noreply.github.com>
2019-10-28 23:02:34 -0600

    2019/10/28

--- misitio/wp-config.php
+++ misitio/wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'wp-blog-2019' );
 
 /** MySQL database username */
-define( 'DB_USER', 'yilung' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'mokouru4545' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'U=+<X;earq!#K1K4(rRTii`@M)7)_~zg?76&L$9AO-z!9HihdTMRdABu,T;DS>#/' );
-define( 'SECURE_AUTH_KEY',  '^XB3|J>C7~zhe$yGaIKV`>YF^vEx]b>uJTyDYOlI~FQJpLKL9#FRQV4,l.)db2cf' );
-define( 'LOGGED_IN_KEY',    '>)g.j#tx?3=Kc(m:T+gf  #E@?&Q4{SCTqN!$.:sYxj9h[p/_29!owYz}T4E@uT:' );
-define( 'NONCE_KEY',        '5#Qn+g$xDG*V5CT3jEDX~T_(H),|ZW9Rqt:?UEEw Kz~I_A@L3uFzYI7d/br, +8' );
-define( 'AUTH_SALT',        '8J3Aq}TkQ$N{,;f@LaEXfs;wocsXk(wGHav/nS]>#Hu,(%F)ja{}|jS!g!m!Z$~2' );
-define( 'SECURE_AUTH_SALT', '~2A8~*DLV]HD%+_:()f8 {4l)yqZg])`I|}e=H<WF0CEHJ7:m>z+&;~;mJU8x_.4' );
-define( 'LOGGED_IN_SALT',   '~?oZcL0Y_yy$E9*Q,9]`s#-S[w*$}RzOxVCA&foSvbM$AWG)w53=oa|5kkE|mGF?' );
-define( 'NONCE_SALT',       '?GRi5TxR4T,~TN[,iuskyFoIHi.=D<#sjIPD4fe/x6Mi`A0css,vaXb!kCY7ZA8q' );
+define( 'AUTH_KEY',         '+_apZyckl=ke)3pb(1JVGK+v <y6 Gqc==?d@DOHH`]jPbZUF3j?D:KDcb#/e0/Y' );
+define( 'SECURE_AUTH_KEY',  'clPR>21//2N^R^5lx$vq%|oJ]!|_~$I%|Us-rza-h(Zd!<E-S[~VoWKv#YTa@N&z' );
+define( 'LOGGED_IN_KEY',    'Bsq^Kr!kuPczql3&/9D#`wq7fbl%;wQ=1?ZSfW#iI8=7XjYH|f[!$x4apo%*gk(K' );
+define( 'NONCE_KEY',        '|wHs57+oROcEENonK6I9k,Td6.gwipx!E}FtRCP )Sd}ABRpH(I@5N$qJ74RQA;@' );
+define( 'AUTH_SALT',        'jz^d5LRG-g4hsH=k$k,wbk[(M<NM~bEr%on23~<a_S4IhH+ 3EDiOCQ)&f)8,K.T' );
+define( 'SECURE_AUTH_SALT', 'Y_=:=~YQrU*g?Ga8Y|#W$=U*E3A1pg[et}Ib5w3~iR5s+og&U;~TpdkIY$!lAr{w' );
+define( 'LOGGED_IN_SALT',   'Id<j*23Xh!lR?2UnMdmB/}KOP@j)Z[V4K#LRGNpE;zxOba3X.JZaoebFkJ|12LL<' );
+define( 'NONCE_SALT',       'X Q>V:r_`y|?3-d.2i23nf_r3BgE2(?hmm/}sz?6w1X$B.LZ36Jf$w_R-O}Gk!`Q' );
 
 /**#@-*/
 
@@ -79,8 +79,6 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 
-	
-define('FS_METHOD', 'direct');
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */

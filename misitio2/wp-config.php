9906c344a2b4f2b03fc375d13510393f105a1059
YiLung1013 <56326995+YiLung1013@users.noreply.github.com>
2019-11-04 22:48:06 -0600

    2019/11/4

--- misitio2/wp-config.php
+++ misitio2/wp-config.php
@@ -32,7 +32,7 @@ define( 'DB_PASSWORD', 'mokouru4545' );
 define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_CHARSET', 'latin1_swedish_ci' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
